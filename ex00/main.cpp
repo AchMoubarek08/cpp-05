275ac2babd1683d30adb90f9e9a40664da48fc89
AchMoubarek08 <amoubare@DESKTOP-IGQBOJV>
2023-02-09 20:46:54 +0100

    01/02OK

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     try
     {
-        Bureaucrat b("leomessi", 1);
+        Bureaucrat b("leomessi", 101);
         std::cout << b;
         for(int i = 0; i < 50; i++)
             b.decrementGrade();
